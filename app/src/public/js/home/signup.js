614ecc230a2a4689cd2fa5ed319e2730acbdf866
SANGHYO LEE <adsff123@gmail.com>
2022-04-08 14:59:30 +0900

    회원가입 구현2

--- app/src/public/js/home/signup.js
+++ app/src/public/js/home/signup.js
@@ -5,39 +5,42 @@ const password = document.querySelector("#password");
 const passwordCheck = document.querySelector("#password-check");
 const name = document.querySelector("#name");
 const email = document.querySelector("#email");
+
 const signupbtn = document.querySelector("#signupbtn");
 
-signupbtn.addEventListener("click ", signup); 
- 
+signupbtn.addEventListener("click", signup);
+
 function signup() {
-    const req = {
-        id: id.value,
-        password: password.value, 
-        passwordChec: passwordCheck.value,
-        name: name.value,
-        email: email.value, 
-    };
+  const req = {
+    id: id.value,
+    password: password.value, 
+    passwordCheck: passwordCheck.value,
+    name: name.value,
+    email: email.value,
+  };
+
     console.log(req);
+    
 
-    fetch("/signup", {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(req), 
-    }).then((res) => res.json()) 
-      .then((res) => {
-          if (res.success){
-              location.href = '/signin';
-          }
-          else {
-              password.focus();
-              alert(res.msg);
-          }
+    // fetch("/signup", {
+    //     method: "post",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(req), 
+    // }).then((res) => res.json()) 
+    //   .then((res) => {
+    //       if (res.success){
+    //           location.href = '/signin';
+    //       }
+    //       else {
+    //           password.focus();
+    //           alert(res.msg);
+    //       }
 
-      })
-      .catch((err)=> {
-           console.error(new Error("회원가입 중 에러 발생")); 
-      });
+    //   })
+    //   .catch((err)=> {
+    //        console.error(new Error("회원가입 중 에러 발생")); 
+    //   });
      
 }
