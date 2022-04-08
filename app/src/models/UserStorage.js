0b86c1d8dc8bffdd6158676756a8a54e2e512595
SANGHYO LEE <adsff123@gmail.com>
2022-04-08 17:08:41 +0900

    filesystem 1

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,12 @@
 "use strict";
 
+const fs = require("fs");
+
+
 class UserStorage {
-    static #users = {
-        db_id: ["adsff123", "mike", "josh", "joey", "danny", "heather","soo","kevin"],
-        db_password: ["1234", "1234", "1234", "1234", "1234", "1234","1234","1234"], 
-        db_name: ["상효","마이크","조쉬","조이","대니","헤더","수","케빈"],
-        db_email: ["abcd@gmail.com"],
-    };
 
     static getUsers(...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -20,19 +17,24 @@ class UserStorage {
     };
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.db_id.indexOf(id);
-        const userKeys = Object.keys(users);
-        const userInfo = userKeys.reduce((newUsers, info)=>{
-            newUsers[info] = users[info][idx];
-            return newUsers;
-        }, {});
+        // const users = this.#users;
+        fs.readFile("./src/database/users.json", (err,data)=>{
+            if (err) throw err;
+            const users = JSON.parse(data);
+            const idx = users.db_id.indexOf(id);
+            const userKeys = Object.keys(users);
+            const userInfo = userKeys.reduce((newUsers, info)=>{
+                newUsers[info] = users[info][idx];
+                return newUsers;
+            }, {}); 
+            
+            return userInfo; 
+        })
         
-        return userInfo; 
     };
 
     static save(userInfo) { 
-        const users = this.#users;
+        // const users = this.#users; 
         users.db_id.push(userInfo.id);
         users.db_password.push(userInfo.password);
         users.db_name.push(userInfo.name);
